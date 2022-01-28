999f348a1d9dd0684e37c2e1fea13bbbf6fb5c7a
Sebastião Teixeira <38736835+sebastiaoteixeira@users.noreply.github.com>
2022-01-28 15:39:00 +0000

    Python package (#3)
    * Add description
    
    * Add installation and run to README
    
    * Add requirements.txt
    
    * Update setup.py

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# HDPasswordGenerator
+# Hierarchical-Deterministic Password Generator
+HDPasswordGenerator is a password generator and manager that offers a solution for recovering passwords on another machine if they are lost. The technique used for this is based on the bip32 and bip39 standards used in cryptocurrency wallets (described below) for the generation of deterministic and hierarchical keys.
+
+### Compatibility
+UNIX-like systems
+
+## Installation
+    # Clone repository from github
+    git clone https://github.com/sebastiaoteixeira/HDPasswordGenerator.git --recursive
+    
+    # Install HDPasswordGenerator
+    sudo python3 setup.py install
+
+## Run
+    hdpassgenerator
--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
+#
+# Hierarchical Deterministic Password Generator
+# Copyright (C) 2022 Sebastião Teixeira
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from setuptools import setup
 import requests
 import os
 
-print()
 # Download rockyou list
+print("Downloading...\nIt may take a few minutes")
 url = 'https://gateway.pinata.cloud/ipfs/QmPvcDGMmq6CPGAAQHhGfpp6XQdqTV7SqhbZPhCDwDrVcF/rockyou.txt'
-myfile = requests.get(url)
-open(os.path.dirname(os.path.join(os.path.abspath(__file__), 'src/hdpassgenerator/rockyou.txt')), 'wb').write(myfile.content)
+rockyou = requests.get(url)
+open(__file__[:(len(__file__)-8)] + 'src/hdpassgenerator/rockyou.txt', 'wb').write(rockyou.content)
 
 
 setup(
